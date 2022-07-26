ec741b0de6d41a94b00fca71ec90e2940e1fbb20
Salvador Borit <5084@holbertonschool.com>
2022-07-26 09:21:34 -0300

    readme and header file

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <
+
+#endif /* MAIN_H */
